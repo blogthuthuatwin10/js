646931333a96240385725710050640fa7258d94c
Nguyễn Tuấn <nguyenanhtuan2401@gmail.com>
2018-01-05 15:52:41 +0700

    Add files via upload

---
+++ notifications.js
@@ -0,0 +1,32 @@
+﻿importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAqRXfBZk1h3d-aiWaKb3e38mIyUwshrmI",
+  authDomain: "nguyentuan-3d955.firebaseapp.com",
+  databaseURL: "https://nguyentuan-3d955.firebaseio.com",
+  projectId: "nguyentuan-3d955",
+  storageBucket: "nguyentuan-3d955.appspot.com",
+  messagingSenderId: "227881048794"
+};
+firebase.initializeApp(config);
+
+const messaging = firebase.messaging();
+
+messaging.requestPermission()
+.then(function() {
+  return messaging.getToken();
+ })
+.then(function(token) {
+// send rest call to add to database
+$.ajax('https://nguyentuan-3d955.firebaseio.com/pushtokens/'+token+'.json', {
+  method: 'PUT',
+  data: 'true',
+  error: function(err) {
+    }
+  });
+})
+.catch(function(err) {
+  console.log('Permission denied');
+});
