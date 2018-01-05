00c58ddaef39c8df56d4eb49d0385fbf2cb14b3b
Nguyễn Tuấn <nguyenanhtuan2401@gmail.com>
2018-01-05 18:34:58 +0700

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,27 @@
+﻿if( 'undefined' === typeof window){
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
+firebase.initializeApp({
+  'messagingSenderId': '227881048794'
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: 'https://i.imgur.com/F5xk7Ah.png'
+  };
+
+  return self.registration.showNotification(notificationTitle,
+      notificationOptions);
+});
+}
